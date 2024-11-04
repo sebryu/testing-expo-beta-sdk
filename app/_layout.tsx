f86be8388872bd797768f5e02464d906b3afcc4c
Sebastian Szewczyk <sebastian.szewczyk@swmansion.com>
2024-11-04 10:33:25 +0100

    Added expo dev client

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import 'expo-dev-client';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
